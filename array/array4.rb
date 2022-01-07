9bc5042760181adb0ea6791afbc26fd1fde19dc4
Sonali Kar <sonali.kar87@gmail.com>
2022-01-07 13:12:07 +0000

    Array Execise - 2

---
+++ array/array4.rb
@@ -0,0 +1,3 @@
+#create a new array of these names in block caps ["Sandy", "Terrence","Susan", "Humphrey", "Simone", "Englebert", "Imogen", "Ron"]
+names = ["Sandy", "Terrence","Susan", "Humphrey", "Simone", "Englebert", "Imogen", "Ron"]
+puts names.map { |name| name.upcase }
